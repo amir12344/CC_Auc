d836fa28f6f3ab829f83ba8996803f9032553ef9
Amir Sayyad <amir@commercecentral.ai>
2025-06-02 10:54:54 +0000

    Adding amplify configure file

---
+++ src/app/ConfigureAmplifyClientSide.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Amplify } from 'aws-amplify';
+
+import outputs from '../../amplify_outputs.json'; 
+
+  Amplify.configure(outputs, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  // This component doesn't render anything, it's just for executing Amplify.configure().
+  return null;
+} 
