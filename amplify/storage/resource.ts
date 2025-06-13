018988b7059f85890b541c40c544bb67c24c1c34
Amir Sayyad <amir@commercecentral.ai>
2025-06-13 14:40:58 +0000

    fix: remove all storage access rules when auth is undefined

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,14 +2,14 @@ import { defineStorage } from '@aws-amplify/backend'
 
 export const storage = defineStorage({
   name: 'commerceCentralStorage',
-  access: (allow) => ({
-    'ReSellCertificates/private/{entity_id}/*': [
-      // Temporarily allow guest access - VERY PERMISSIVE!
-      allow.guest.to(['read', 'write', 'delete']),
-    ],
-    'ReSellCertificates/public/*': [
-      // Temporarily allow guest access
-      allow.guest.to(['read']),
-    ],
-  }),
+//   access: (allow) => ({
+//     'ReSellCertificates/private/{entity_id}/*': [
+//       // Temporarily allow guest access - VERY PERMISSIVE!
+//       allow.guest.to(['read', 'write', 'delete']),
+//     ],
+//     'ReSellCertificates/public/*': [
+//       // Temporarily allow guest access
+//       allow.guest.to(['read']),
+//     ],
+//   }),
 })
